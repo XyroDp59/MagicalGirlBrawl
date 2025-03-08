f0e191907a09fb9d004bb9804a76076538a80c84
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-03-08 15:17:31 +0100

    Push changes to calm rider down

--- MagicalGirlBrawl/Assets/Scripts/Movement.cs
+++ MagicalGirlBrawl/Assets/Scripts/Movement.cs
@@ -10,13 +10,18 @@ public class Movement : MonoBehaviour
     [SerializeField] private float move_speed = 7f;
     [SerializeField] private float jump_power = 17f;
     [SerializeField] public int nb_double_jump = 2;
-    private PlayerInput _playerInput;
+    //private PlayerInput _playerInput;
+    private int _walkBoolHash = Animator.StringToHash("Walking");
+    private Animator _animator;
+    private float _defaultLocalXScale;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        _playerInput = GetComponent<PlayerInput>();
+        //_playerInput = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
+        _defaultLocalXScale = transform.localScale.x;
     }
 
     private void OnJump()
@@ -29,8 +34,17 @@ public class Movement : MonoBehaviour
     private void OnMove(InputValue value)
     {
         direction = value.Get<Vector2>().x;
+        _animator.SetBool(_walkBoolHash, direction != 0);
+        float newLocalXScale = transform.localScale.x;
+        if (direction < 0) newLocalXScale = - _defaultLocalXScale;
+        if (direction > 0) newLocalXScale = _defaultLocalXScale;
+        transform.localScale = new Vector3(newLocalXScale, transform.localScale.y, transform.localScale.z);
     }
 
+    private void OnPrevious()
+    {
+        Debug.Log("here");
+    }
     public void Reset_Double_Jump_Ground()
     {
         nb_double_jump = 2;
