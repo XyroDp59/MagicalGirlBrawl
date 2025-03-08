a5b6b64ae867527ebdbb7e075a578e3988d960c5
Titouan DELEPORTE <titouandeleporte@icloud.com>
2025-03-08 18:17:09 +0100

    Bullets
    Bullets détruites après 10s

--- MagicalGirlBrawl/Assets/Scripts/ProjectileBehaviour.cs
+++ MagicalGirlBrawl/Assets/Scripts/ProjectileBehaviour.cs
@@ -8,7 +8,7 @@ public class ProjectileBehaviour : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Destroy(gameObject,10f);
     }
 
     // Update is called once per frame
