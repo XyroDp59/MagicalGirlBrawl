dcea1ece02738e1fffb424e50e290fbbd82cd002
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-03-08 12:07:17 +0100

    Pushing stuff that actually compiles

--- MagicalGirlBrawl/Assets/Scripts/DeviceDetection.cs
+++ MagicalGirlBrawl/Assets/Scripts/DeviceDetection.cs
@@ -17,7 +17,7 @@ public class DeviceDetection : MonoBehaviour
             if (gamepad.wasUpdatedThisFrame && !CurrentDevices.Contains(gamepad.device))
             {
                 CurrentDevices.Add(gamepad.device);
-                unboundMovements[0].BindDevice(gamepad.device);
+                //unboundMovements[0].BindDevice(gamepad.device);
                 unboundMovements.RemoveAt(0);
                 Debug.Log("new device - controller");
             }
@@ -28,7 +28,7 @@ public class DeviceDetection : MonoBehaviour
             if (joystick.wasUpdatedThisFrame && !CurrentDevices.Contains(joystick.device))
             {
                 CurrentDevices.Add(joystick.device);
-                unboundMovements[0].BindDevice(joystick.device);
+                //unboundMovements[0].BindDevice(joystick.device);
                 unboundMovements.RemoveAt(0);
                 Debug.Log("new device - joystick");
             }
@@ -37,7 +37,7 @@ public class DeviceDetection : MonoBehaviour
         if (Keyboard.current.anyKey.isPressed && !CurrentDevices.Contains(Keyboard.current.device))
         {
             CurrentDevices.Add(Keyboard.current.device);
-            unboundMovements[0].BindDevice(Keyboard.current.device);
+            //unboundMovements[0].BindDevice(Keyboard.current.device);
             unboundMovements.RemoveAt(0);
             Debug.Log("new device - keyboard");
         }
