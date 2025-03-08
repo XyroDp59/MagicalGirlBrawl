cd8f9b96aaedbcab80b846b515a34c7b73443a27
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-03-08 12:06:04 +0100

    Import examples

--- MagicalGirlBrawl/Assets/Scripts/DeviceDetection.cs
+++ MagicalGirlBrawl/Assets/Scripts/DeviceDetection.cs
@@ -5,21 +5,40 @@ using UnityEngine.InputSystem;
 public class DeviceDetection : MonoBehaviour
 {
     public List<InputDevice> CurrentDevices = new List<InputDevice>();
+    public List<Movement> unboundMovements = new List<Movement>();
+    
 
     private void Update()
     {
-        foreach(var g in Gamepad.all)
+        if (unboundMovements.Count == 0) return;
+        
+        foreach(var gamepad in Gamepad.all)
         {
-            if (g.wasUpdatedThisFrame && !CurrentDevices.Contains(g.device))
+            if (gamepad.wasUpdatedThisFrame && !CurrentDevices.Contains(gamepad.device))
             {
-                CurrentDevices.Add(g.device);
+                CurrentDevices.Add(gamepad.device);
+                unboundMovements[0].BindDevice(gamepad.device);
+                unboundMovements.RemoveAt(0);
                 Debug.Log("new device - controller");
+            }
+        }
+
+        foreach (var joystick in Joystick.all)
+        {
+            if (joystick.wasUpdatedThisFrame && !CurrentDevices.Contains(joystick.device))
+            {
+                CurrentDevices.Add(joystick.device);
+                unboundMovements[0].BindDevice(joystick.device);
+                unboundMovements.RemoveAt(0);
+                Debug.Log("new device - joystick");
             }
         }
 
         if (Keyboard.current.anyKey.isPressed && !CurrentDevices.Contains(Keyboard.current.device))
         {
             CurrentDevices.Add(Keyboard.current.device);
+            unboundMovements[0].BindDevice(Keyboard.current.device);
+            unboundMovements.RemoveAt(0);
             Debug.Log("new device - keyboard");
         }
     }
